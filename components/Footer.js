ea1f617069335d25ef187c811e4768aeb7d374c7
dhooa <arazza1693@gmail.com>
2025-06-28 02:17:55 +0700

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-green-800 text-white text-center p-4">
+      &copy; 2025 Green Coffee Shelter. All rights reserved.
+    </footer>
+  );
+}
